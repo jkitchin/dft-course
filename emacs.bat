be7ebbfdd614cce50fdd7f039cce9312e3f3cff4
John Kitchin <jkitchin@cmu.edu>
2012-09-01 15:29:47 -0400

    updated path to init file

--- emacs.bat
+++ emacs.bat
@@ -1,4 +1,1 @@
-set EMACSROOT=C:\Users\jkitchin\emacs-24.1\bin\
-set DFTCOURSE=C:\Users\jkitchin\Dropbox\CMU\classes\06-640-Molecular-Simulations-Fall-2012\dft-course
-
-%EMACSROOT%\runemacs.exe -q -l %DFTCOURSE%\dft-course-init.el
+"C:\Program Files (x86)\GNU Emacs 24.1\bin\runemacs.exe" -q -l %DFTCOURSE%\emacs\dft-course-init.el
