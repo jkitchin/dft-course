0c1056b3ca4d18e234b6e433d2a5f0954fe268bd
John Kitchin <jkitchin@cmu.edu>
2012-09-01 13:12:13 -0400

    create windows batch file to launch emacs

---
+++ emacs.bat
@@ -0,0 +1,4 @@
+set EMACSROOT=C:\Users\jkitchin\emacs-24.1\bin\
+set DFTCOURSE=C:\Users\jkitchin\Dropbox\CMU\classes\06-640-Molecular-Simulations-Fall-2012\dft-course
+
+%EMACSROOT%\runemacs.exe -q -l %DFTCOURSE%\init.el
