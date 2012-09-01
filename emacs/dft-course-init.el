69b731bf0b8e39b32bdec03d71d79829a1d6714d
John Kitchin <jkitchin@cmu.edu>
2012-09-01 15:30:41 -0400

    moved init file into here

---
+++ emacs/dft-course-init.el
@@ -0,0 +1,41 @@
+;; turn on font-lock mode
+(global-font-lock-mode t)
+
+(show-paren-mode 1)
+(line-number-mode 1)
+
+;disable backup
+(setq backup-inhibited t)
+
+(add-to-list 'load-path
+             (concat (getenv "DFTCOURSE") "/emacs/python-mode.el-6.0.11/"))
+(setq py-install-directory
+             (concat (getenv "DFTCOURSE") "/emacs/python-mode.el-6.0.11/"))
+
+(setq py-shell-name "C:/Python27/Scripts/ipython.bat")
+(require 'python-mode)
+
+; register python in org-mode
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python . t)))
+
+; enable prompt-free code running
+(setq org-confirm-babel-evaluate nil)
+
+; no extra indentation
+(setq org-src-preserve-indentation t)
+(setq org-startup-with-inline-images "inlineimages")
+
+; use syntax highlighting in org-file
+(setq org-src-fontify-natively t)
+
+; set default :results to output
+(setq org-babel-default-header-args
+      (cons '(:results . "output")
+	    (assq-delete-all :results org-babel-default-header-args)))
+
+; you can further customize by creating a new lisp file and
+;uncommenting this line
+; (load-file "yourcustom.el")
