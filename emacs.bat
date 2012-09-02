5c0c0670dc8f2bc9ab505bd15ab0f00e521017c5
John Kitchin <jkitchin@cmu.edu>
2012-09-02 14:17:19 -0400

    changed path to use dft-course emacs

--- emacs.bat
+++ emacs.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\GNU Emacs 24.1\bin\runemacs.exe" -q -l %DFTCOURSE%\emacs\dft-course-init.el
+%DFTCOURSE%\emacs\Emacs-24\bin\runemacs.exe -q -l %DFTCOURSE%\emacs\dft-course-init.el
