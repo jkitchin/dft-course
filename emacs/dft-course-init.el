163ef3af4f5a0e1cadb96c27bb2bb50eb41989e0
John Kitchin <jkitchin@cmu.edu>
2012-09-02 07:33:43 -0400

    added color theme, and my org customizations

--- emacs/dft-course-init.el
+++ emacs/dft-course-init.el
@@ -1,3 +1,8 @@
+
+;; create an environment variable called DFTCOURSE that points to where the course files are.
+(setq dft-course (getenv "DFTCOURSE"))
+
+
 ;; turn on font-lock mode
 (global-font-lock-mode t)
 
@@ -7,13 +12,59 @@
 ;disable backup
 (setq backup-inhibited t)
 
+;; answer with y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq inhibit-startup-screen t)
+
+;; Color themes and frame title
+(setq frame-title-format "Emacs for dft-course")
+
+(add-to-list 'load-path (concat dft-course "/emacs/color-theme-6.6.0"))
+(require 'color-theme)
+(load-file (concat (getenv "DFTCOURSE") "/emacs/my-color-theme.el"))
+(my-color-theme)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; python
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path
-             (concat (getenv "DFTCOURSE") "/emacs/python-mode.el-6.0.11/"))
-(setq py-install-directory
-             (concat (getenv "DFTCOURSE") "/emacs/python-mode.el-6.0.11/"))
+             (concat (getenv "DFTCOURSE") "/emacs/python-mode"))
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 (setq py-shell-name "C:/Python27/Scripts/ipython.bat")
+(setq py-python-command-args '(" --colors=LightBG --pylab"))
+;(setq py-python-command-args '("-pylab" "-colors" "LightBG"))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default py-indent-offset 4)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (require 'python-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'org-install)
+(require 'org-list)
+(require 'org-special-blocks)
+(require 'org)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key [f12] 'org-mode)
+
+(global-set-key "\C-e" 'end-of-line); overwrites org-mode \C-e definition
+
+(global-set-key "\C-cL" 'org-insert-link-global)
+(global-set-key "\C-co" 'org-open-at-point-global)
+
 
 ; register python in org-mode
 (org-babel-do-load-languages
@@ -35,6 +86,142 @@
 (setq org-babel-default-header-args
       (cons '(:results . "output")
 	    (assq-delete-all :results org-babel-default-header-args)))
+
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
+  )
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
+
+
+(setq org-export-latex-default-packages-alist
+      (quote
+       (("AUTO" "inputenc" t)
+	("" "fixltx2e" nil)
+    ("" "url")
+	("" "graphicx" t)
+    ("" "minted" t)
+    ("" "color" t)
+	("" "longtable" nil)
+	("" "float" nil)
+	("" "wrapfig" nil)
+	("" "soul" t)
+	("" "textcomp" t)
+    ("" "amsmath" t)
+	("" "marvosym" t)
+	("" "wasysym" t)
+	("" "latexsym" t)
+	("" "amssymb" t)
+	("linktocpage,
+  pdfstartview=FitH,
+  colorlinks,
+  linkcolor=blue,
+  anchorcolor=blue,
+  citecolor=blue,
+  filecolor=blue,
+  menucolor=blue,
+  urlcolor=blue" "hyperref" t)
+	("" "attachfile" t)
+	"\\tolerance=1000")))
+
+(setq org-export-latex-listings 'minted)
+(setq org-export-latex-minted-options
+           '(("frame" "lines")
+             ("fontsize" "\\scriptsize")
+             ("linenos" "")))
+(setq org-latex-to-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "bibtex %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+;;; new link formats for org-mode that I wrote
+
+
+; here is a way to get pydoc in a link: [[pydoc:numpy]]
+(setq org-link-abbrev-alist
+      '(("pydoc" . "shell:pydoc %s")))
+
+;; these allow me to write mod:numpy or func:numpy.dot to get
+;; clickable links to documentation
+(org-add-link-type
+ "mod"
+ (lambda (arg)
+   (shell-command (format "pydoc %s" arg) nil))
+ (lambda (path desc format)
+   (cond
+    ((eq format 'latex)
+     (format "\\texttt{%s}" path)))))
+
+(org-add-link-type
+ "func"
+ (lambda (arg)
+   (shell-command (format "pydoc %s" arg) nil))
+ (lambda (path desc format)
+   (cond
+    ((eq format 'latex)
+     (format "\\texttt{%s}" path)))))
+
+;;; support for links to microsoft docx,pptx,xlsx files
+;;; standard org-mode opens these as zip-files
+;;  http://orgmode.org/manual/Adding-hyperlink-types.html
+(org-add-link-type "msx" 'org-msx-open)
+
+(defun org-msx-open (path)
+       "Visit the msx file on PATH.
+
+uses the dos command:
+start  empty title path
+"
+       (shell-command
+	(concat "start \"title\" " (shell-quote-argument path)) t))
+
+
+(org-add-link-type "ashell" 'org-ashell-open)
+(defun org-ashell-open (cmd)
+"open an ashell:cmd link
+[[ashell:xterm -e \"cd 0; ls && /bin/bash\"]]
+
+I use this to run commands asynchronously in the shell. org-mode runs shell links in a blocking mode, which is annoying when you open an xterm."
+(start-process-shell-command "ashell" "*scratch*" cmd))
+
+
+;; -*- emacs-lisp -*-   [[color:red][in red]]
+(org-add-link-type
+ "color"
+ (lambda (path)
+   (message (concat "color "
+		    (progn (add-text-properties
+			    0 (length path)
+			    (list 'face `((t (:foreground ,path))))
+			    path) path))))
+ (lambda (path desc format)
+   (cond
+    ((eq format 'html)
+     (format "<span style=\"color:%s;\">%s</span>" path desc))
+    ((eq format 'latex)
+     (format "{\\color{%s}%s}" path desc)))))
+
+;; -*- emacs-lisp -*-   [[incar:keyword]]
+; this makes nice links in org-mode to the online documentation and
+; renders useful links in output
+(org-add-link-type
+ "incar"
+  (lambda (keyword)
+    (shell-command (format "firefox http://cms.mpi.univie.ac.at/wiki/index.php/%s" keyword) nil))
+  ; this function is for formatting
+  (lambda (keyword link format)
+   (cond
+    ((eq format 'html)
+     (format "<a href=http://cms.mpi.univie.ac.at/wiki/index.php/%s>%s</a>" keyword keyword))
+    ((eq format 'latex)
+     (format "\\href{http://cms.mpi.univie.ac.at/wiki/index.php/%s}{%s}"  keyword keyword)
+
+))))
+
 
 ; you can further customize by creating a new lisp file and
 ;uncommenting this line
